dfd3a68d68a6f729cceffdbf993d24f2cee895df
poly1234 <methesteam@gmail.com>
2026-05-18 11:46:04 +0900

    Add files via upload

---
+++ Bank/console_bank.py
@@ -0,0 +1,68 @@
+from account.account import AccountService
+
+def get_input_int(text:str):
+    while True:
+        try:
+            return int(input(text))
+        except:
+            print("숫자만 입력 가능합니다. 다시 입력해주세요.")
+
+def get_input_int_list(text:str, arr:list):
+    try:
+        temp = int(input(text))
+        if not temp in arr:
+            temp = False
+    except:
+        temp = False
+    return temp
+
+# 메뉴와 사용자 interaction에 따른 서비스 호출
+def select_menu():
+    print("=======================================================")
+    print(" 1. 계좌생성 | 2. 계좌목록 | 3. 입금 | 4. 출금 | 0. 종료")
+    print("=======================================================")
+    menu = get_input_int_list(">> 메뉴 선택 : ", list(range(5)))
+    return menu
+
+def start_console_bank(aservice):
+    print()
+    print("========== Bank ==========")
+
+    while True:
+        menu = select_menu()
+        if menu == 0: # 종료
+            break
+        elif menu == 1: # 계좌 생성
+            # 계좌번호, 계좌주, 잔액 입력을 받아서 계좌 생성
+            account_no = input(">> 계좌번호 : ")
+            owner = input(">> 계좌주 : ")
+            balance = get_input_int(">> 초기 입금액 : ")
+            if aservice.create_account(account_no, owner, balance):
+                print("결과 : 계좌가 생성되었습니다.")
+        elif menu == 2: # 계좌 목록
+            account_list = aservice.list_account()
+            print("----------")
+            print(" 계좌 목록 ")
+            print("----------")
+            for account in account_list:
+                print(account)
+        elif menu == 3: # 입금
+            print("----------")
+            print("   예금   ")
+            print("----------")
+            account_no = input(">> 계좌번호 : ")
+            amount = get_input_int(">> 예금액 : ")
+            aservice.deposit(account_no, amount) 
+        elif menu == 4: # 출금
+            print("----------")
+            print("   출금   ")
+            print("----------")
+            account_no = input(">> 계좌번호 : ")
+            amount = get_input_int(">> 출금액 : ")
+            aservice.withdraw(account_no, amount)
+
+    print("========== 이용해 주셔서 감사합니다. ==========")
+
+if __name__ == "__main__":
+    aservice = AccountService()
+    start_console_bank(aservice)
---
+++ Member/member/member.py
@@ -0,0 +1,65 @@
+class Member:
+    def __init__(self, member_num:int, id:str, pw:str, name:str, phone_num:int, address:str):
+        self.__member_num = member_num
+        self.__id = id
+        self.__pw = pw
+        self.__name = name
+        self.__phone_num = phone_num
+        self.__address = address
+
+    def __str__(self):
+        return "회원번호: " + str(self.__member_num) + " | 아이디: " + self.__id + " | 비밀번호: " + self.__pw + " | 이름: " + self.__name + " | 전화번호: " + str(self.__phone_num) + " | 주소: " + self.__address
+
+    def get_member_num(self):
+        return self.__member_num
+    
+    def get_id(self):
+        return self.__id
+    
+    def get_pw(self):
+        return self.__pw
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_phone_num(self):
+        return self.__phone_num
+    
+    def get_address(self):
+        return self.__address
+    
+class MemberService:
+    def __init__(self):
+        self.__member_list = []
+
+    def register(self, member_num, id, pw, name, phone_num, address):
+        newMember = Member(member_num, id, pw, name, phone_num, address)
+        self.__member_list.append(newMember)
+        return True
+
+    def list_memeber(self):
+        return self.__member_list
+    
+    def member_info(self, member_num:int):
+        for member in self.__member_list:
+            if member.get_member_num() == member_num:
+                return member
+        return False
+
+    def modify_member_info(self, member_num:int, new_member_num, new_id, new_pw, new_name, new_phone_num, new_address):
+        for member in self.__member_list:
+            if member.get_member_num() == member_num:
+                i = self.__member_list.index(member)
+                self.__member_list[i] = Member(new_member_num, new_id, new_pw, new_name, new_phone_num, new_address)
+                return True
+        return False
+
+    def unregister(self, member_num:int):
+        for member in self.__member_list:
+            if member.get_member_num() == member_num:
+                try:
+                    self.__member_list.remove(member)
+                    return True
+                except:
+                    return False
+        return False
